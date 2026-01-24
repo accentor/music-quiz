2e06b17015fe864e3de23b2a4fab6e67d81121a4
Robbe Van Petegem <git@robbevp.be>
2026-01-24 10:09:11 +0100

    Upgrade eslint config to flat config

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import eslint from "@eslint/js";
+import prettierPlugin from "eslint-plugin-prettier/recommended";
+import pluginVue from "eslint-plugin-vue";
+import globals from "globals";
+
+export default [
+  eslint.configs.recommended,
+  ...pluginVue.configs["flat/recommended"],
+  {
+    rules: {
+      curly: "warn",
+      "no-console": "warn",
+      "no-debugger": "warn",
+      "vue/multi-word-component-names": "off",
+    },
+    languageOptions: {
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+  prettierPlugin,
+];
